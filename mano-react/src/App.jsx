11c3346b9d8f0d3c93ab4b2ecfa5fc444f2fe6b0
KarolisSutas <sutaskarolis@gmail.com>
2025-09-10 14:07:52 +0300

    react 002 state

--- mano-react/src/App.jsx
+++ mano-react/src/App.jsx
@@ -1,46 +1,109 @@
-import './App.css'
+import { useState } from 'react';
+import './App.scss';
+import './buttons.scss';
+import View from './Components/071/view';
 import randomColor from './Functions/randomColor';
-import random from './Functions/random';
-import Bebras from './Components/070/Bebras';
-import Box from './Components/070/Box';
+
 
 function App() {
 
-  console.log('GO!', random(0, 1));
+    console.log('PASILEIDO');
 
-  let kas = '';
-
-  for (let i = 1; i < 10; i++) {
-    kas += i;
+  const mygtukas1 = _ => {
+    console.log('CLICK 1');
+  }
+  const mygtukas2 = e => {
+    console.log('CLICK 2', e.target.name);
   }
 
-  const daug = _ => {
-    return <h6>DAUGYBE</h6>;
+  const kitiMygtukai = spalva => {
+    console.log('CLICK', spalva);
+
   }
 
-  const spalva = randomColor();
+  const mygtukas3 = (e, kas) => {
+    console.log('CLICK 2', e.target.name, kas);
+  }
+
+  const [counter, setCounter] = useState(1);
+
+  const plus1 = _ => {
+    setCounter(sena => sena + 1)
+    setCounter(sena => sena + 1)
+    setCounter(sena => sena + 1)
+    // setCounter(counter + 1) 
+    // setCounter(counter + 1) 
+    console.log(counter); 
+  }
+
+  const mas1 = [
+    <h3>Vienas <i>1</i></h3>,
+    <h3>du <i>2</i></h3>,
+    <h3>trys <i>3</i></h3>,
+    <h3>keturi <i>4</i></h3>
+  ];
+
+  const mas2 = [
+    {name: 'Vienas', id: 1},
+    {name: 'Du', id: 2},
+    {name: 'Trys', id: 3},
+    {name: 'Keturi', id: 4},
+  ]
+
+  const [sq, setSq] = useState([]);
+
+  const addSq = _ => {
+    setSq(s => [randomColor(), ...s]);
+  }
+
+  const remSq = _ => {
+    setSq(s => {
+        const copy = [...s];
+        copy.shift();
+        return copy;
+    });
+}
+
 
   return (
     <>
-        <Box dydis={random(50, 200)} plotis="200" dar={{
-          spalva: 'skyblue',
-          siena: '17px'
-        }}/><Box dydis="55" plotis="300" dar={{
-          spalva: 'crimson',
-          siena: '7px'
-        }}/>
-        <Bebras/><Bebras/><Bebras/><Bebras/><Bebras/>
+        <button className="green" onClick={mygtukas1}>GO</button>
+        <button className="red" name="raudonas" onClick={mygtukas2}>GO</button>
 
-        <h1 style={
-          { color: spalva,
-            fontFamily: 'monospace',
-            letterSpacing: '5px'
-           }
-        }>REACT - {kas}</h1>
-        <Bebras/><Bebras/>
-        <h2 style={{color:spalva}}>Matematika 5 + 7 = {5 + 7} </h2>
-        <div>{daug()} {randomColor()} </div>
-        {random(0, 1) ? <h3>TAIP {randomColor()}</h3> : <h3>NE</h3>}
+        <button className="yellow" onClick={_ => kitiMygtukai('Geltonas')}>GO</button>
+        <button className="black" onClick={_ => kitiMygtukai('Juodas')}>GO</button>
+        <button className="blue" onClick={_ => kitiMygtukai('Mėlynas')}>GO</button>
+
+        <button className="teal" name="keista" onClick={e => mygtukas3(e, 'spalva')}>GO</button>
+
+        <hr/>
+
+        <h2>{counter}</h2> 
+        <button className="green" onClick={plus1}>+1</button>
+
+        <hr/>
+
+        {/* {
+            mas2.map((el, i) => <h3 key={i}>{el.name} <i>{el.id}</i></h3>)
+        }
+
+        {
+            mas2.map((el, i) => <View key={i} el={el}/>)
+        } */}
+
+        <div className="sq-bin">
+            {
+            sq.map((el, i) => <div className="sq" key={i} style={{
+                borderColor: el,
+                background: el + '80'
+            }}></div>)
+            }
+        </div>
+        <hr/>
+
+        <button className="blue" onClick={addSq}>+[]</button>
+        <button className="red" onClick={remSq}>+[]</button>
+
     </>
       
   );
