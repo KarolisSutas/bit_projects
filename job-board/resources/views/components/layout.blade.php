9a1011e18feecf4575c67d62d409e592434d4707
KarolisSutas <sutaskarolis@gmail.com>
2025-09-24 22:35:49 +0300

    job-board

--- job-board/resources/views/components/layout.blade.php
+++ job-board/resources/views/components/layout.blade.php
@@ -12,6 +12,35 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-cyan-50 to-emerald-100 text-lime-950">
+    <nav class="mb-8 flex justify-between text-lg font-medium items-center">
+        <ul class="flex space-x-2 items-center">
+          <li>
+            <img class="h-20 w96 object-cover rounded-full border-2 border-lime-600" src="{{ asset('images/lime_logo.png') }}" alt="Logo">
+          </li>
+          <li>
+            <a href="{{ route('jobs.index') }}">LimeTogether</a>
+          </li>
+        </ul>
+    
+        <ul class="flex space-x-4">
+          @auth
+            <li>
+              {{ auth()->user()->name ?? 'Anonymous' }}
+            </li>
+            <li>
+              <form action="{{ route('auth.destroy') }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button class="cursor-pointer">Logout</button>
+              </form>
+            </li>
+          @else
+            <li>
+              <a href="{{ route('auth.create') }}">Sign in</a>
+            </li>
+          @endauth
+        </ul>
+      </nav>
     {{ $slot }}
 </body>
 </html>
